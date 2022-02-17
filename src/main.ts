df7e25c85b2ae809332808371b68d093bfabd3c0
André Wagner <WagnerAndre@JohnDeere.com>
2022-02-17 08:34:16 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const canvas = <HTMLCanvasElement> document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
     const ctx = canvas.getContext("2d");
     ctx.font = "30px Arial";
     ctx.fillText("Hello world!", 10, 50);
