9782e36aba2deb465a3f131c045e08c212d8173b
André Wagner <WagnerAndre@JohnDeere.com>
2022-02-17 09:05:38 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,5 @@ import UI from './ui/ui';
 
 document.addEventListener('DOMContentLoaded', () => {
     const ui = new UI();
-    ui.hello();
-
-    /*
-    const canvas = <HTMLCanvasElement> document.getElementById("canvas");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    const ctx = canvas.getContext("2d");
-    ctx.font = "30px Arial";
-    ctx.fillText("Hello world!", 10, 50);
-
-    const img = new Image();
-    img.onload = () => ctx.drawImage(img, 5, 100);
-    img.src = 'img/warrior.png';
-     */
+    ui.draw();
 });
--- src/ui/ui.ts
+++ src/ui/ui.ts
@@ -1,3 +1,18 @@
 export default class UI {
-    hello() { console.log('hello'); }
+    canvas = <HTMLCanvasElement> document.getElementById("canvas");
+    ctx = this.canvas.getContext("2d");
+
+    constructor() {
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+    }
+
+    draw() {
+        this.ctx.font = "30px Arial";
+        this.ctx.fillText("Hello world!", 10, 50);
+
+        const img = new Image();
+        img.onload = () => this.ctx.drawImage(img, 5, 100);
+        img.src = 'img/warrior.png';
+    }
 }
