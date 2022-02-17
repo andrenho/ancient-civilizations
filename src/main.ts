22b9c0f5d6eec250b4eb34df965b8851734ce74a
André Wagner <WagnerAndre@JohnDeere.com>
2022-02-17 07:55:25 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
-class Hello {
-    print(a: string) {
-        console.log(a);
-    }
-}
-
-new Hello().print('Hello world 3!');
+document.addEventListener('DOMContentLoaded', () => {
+    const canvas = <HTMLCanvasElement> document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.font = "30px Arial";
+    ctx.fillText("Hello world!", 10, 50);
+});
